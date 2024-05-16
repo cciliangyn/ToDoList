5904680b304d2575a49268b2fcc332441c6d0a66
Cecilia Nguyen <cecilianguyen0311@gmail.com>
2024-05-16 03:04:13 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Cecilia Nguyen on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
